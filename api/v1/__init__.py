c6e358f122f0494284bba3a4391c4dd830f93f82
yezi <yehaowei20060411@qq.com>
2023-04-09 08:46:36 +0800

    fix: 修复静态检查报错

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,6 +1,6 @@
 from sanic import Blueprint
 
-from api.v1.split import bp_split
 from api.v1.freq import bp_freq
+from api.v1.split import bp_split
 
 bp_v1 = Blueprint.group(bp_split, bp_freq, url_prefix="/v1")
